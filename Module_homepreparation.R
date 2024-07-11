7e2f2a4b45030df07970298fb17a7a88093997d4
Subhasishbasak <subhasish.basak@anses.fr>
2024-07-11 15:10:11 +0200

    adjust load after home_prep

--- Module_homepreparation.R
+++ Module_homepreparation.R
@@ -6,10 +6,13 @@ data$R_ref <- 10^(data$D_value_beta * data$T_protec + data$D_value_alpha)
 #Prevalence of contaminated servings after cooking 
 data$Prev_home_cook <- data$P_undercook * data$Prev_pproc
 
-# Number of bacteria on one undercooked portion post cook 
+#Number of bacteria on one undercooked portion post cook 
 data$C_home_cook <- data$Prop_protec * 10^(log(data$C_pproc) - data$Time_protec / data$R_ref)
 
-
+#Adjusted bacteria load
+data$C_home_cook <- ifelse(data$C_home_cook <1, 
+                           1, 
+                           data$C_home_cook)
 ## Store outputs
 
 output$Prev_home_cook <- data$Prev_home_cook
