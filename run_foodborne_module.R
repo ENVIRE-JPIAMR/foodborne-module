aa9bf68209e298d1c67ec0072b4ab83090894fbc
Subhasishbasak <subhasish.basak@anses.fr>
2024-07-12 18:05:49 +0200

    results visualization

--- run_foodborne_module.R
+++ run_foodborne_module.R
@@ -4,7 +4,7 @@ source(here::here("utilities/visualization.R"))
 
 ## Initialization
 
-Runs <- 2000 # number of simulation to be performed
+Runs <- 10000 # number of simulation to be performed
 
 # Create dataframe with specified number of rows and column names
 data <- data.frame(matrix(1:Runs, nrow = Runs, ncol = 1))
@@ -26,6 +26,9 @@ source(here::here("Module_homepreparation.R"))
 # Plot outputs
 plot_load(output, plot_all = FALSE)
 plot_prev(output, plot_all = FALSE)
+plot_histogram_and_ecdf(output$C_home_cook, "final load")
+plot_histogram_and_ecdf(output$Prev_home_cook, "final prevalence")
+plot_histogram_and_ecdf(output$prob_carrier*output$Prev_home_cook, "batch risk")
 
 # Save outputs
 write.table(data, file = paste0("data-output/output.csv"), sep = ';', row.names = FALSE, col.names = TRUE)
--- utilities/visualization.R
+++ utilities/visualization.R
@@ -29,31 +29,33 @@ plot_load <- function(data, plot_all = TRUE) {
       group_by(step, color) %>%
       summarize(
         mean_value = mean(log10(value), na.rm = TRUE),
+        median_value = median(log10(value), na.rm = TRUE),
         lower_ci = quantile(log10(value), 0.025, na.rm = TRUE),
         upper_ci = quantile(log10(value), 0.975, na.rm = TRUE),
         .groups = 'drop'
       )
     
-    # Calculate confidence interval width
-    summary_data <- summary_data %>%
-      mutate(ci_width = upper_ci - lower_ci)
-    
     # Create the plot
-    gg <- ggplot(data = summary_data, aes(x = step, y = mean_value, color = color, group = color)) +
-      geom_line(linewidth = 1) +
+    gg <- ggplot(data = summary_data, aes(x = step, color = color, group = color)) +
+      geom_line(aes(y = mean_value, linetype = "Mean"), size = 1, linetype = "solid") +
+      geom_line(aes(y = median_value, linetype = "Median"), size = 1, linetype = "dotted") +
       geom_ribbon(aes(ymin = lower_ci, ymax = upper_ci, fill = color), alpha = 0.3) +  # Confidence bands
       labs(title = "Bacteria load over production stages",
            subtitle = paste0("initial avg. load: ", round(mean(log10(data$load)), 2), " log10 CFU/g and farm prevalence: ", round(mean(data$prev), 2)),
            x = "Stages",
            y = "log10 CFU") +
-      theme_minimal() +
+      scale_linetype_manual(values = c("Mean" = "solid", "Median" = "dotted"), name = "Statistic", labels = c("Mean", "Median")) +
       scale_color_identity() +
-      scale_fill_identity()
-    
+      scale_fill_identity() +
+      theme_minimal() +
+      theme(legend.title = element_blank(),  # Remove legend title
+            legend.text = element_text(size = 12)) +
+      guides(linetype = guide_legend(title = "Statistic", override.aes = list(color = c("black", "black"))))
   }
   
   return(gg)
 }
+
 
 # Function to plot prevalence
 plot_prev <- function(data, plot_all = TRUE) {
@@ -108,4 +110,26 @@ plot_prev <- function(data, plot_all = TRUE) {
   }
   
   return(gg)
+}
+
+# Function for visualizing distribution
+
+plot_histogram_and_ecdf <- function(variable_values, var_name, bins = 10) {
+  # Convert the input list to a data frame
+  data <- data.frame(variable_values)
+  
+  # Create histogram plot
+  histogram_plot <- ggplot(data, aes(x = variable_values)) +
+    geom_histogram(binwidth = NULL, bins = bins, fill = "blue", color = "black", alpha = 0.7) +
+    labs(title = paste("Histogram of", var_name), x = "Variable Values", y = "Frequency") +
+    theme_minimal()
+  
+  # Create ECDF plot
+  ecdf_plot <- ggplot(data, aes(x = variable_values)) +
+    stat_ecdf(geom = "step", color = "blue") +
+    labs(title = paste("Empirical CDF of", var_name), x = "Variable Values", y = "ECDF") +
+    theme_minimal()
+  
+  # Display plots side by side
+  grid.arrange(histogram_plot, ecdf_plot, ncol = 2)
 }
