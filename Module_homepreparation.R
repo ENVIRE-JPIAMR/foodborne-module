3a2da4231d28b0f3878a524ccc7906803d351b62
Subhasishbasak <subhasish.basak@anses.fr>
2024-07-12 18:05:29 +0200

    compute risk

--- Module_homepreparation.R
+++ Module_homepreparation.R
@@ -13,8 +13,12 @@ data$C_home_cook <- data$Prop_protec * 10^(log(data$C_pproc) - data$Time_protec 
 data$C_home_cook <- ifelse(data$C_home_cook <1, 
                            1, 
                            data$C_home_cook)
+##Risk characterization
+data$prob_carrier <- 1 - (1 + (data$C_home_cook/data$DR_omega))^(- data$DR_alpha)
+
 ## Store outputs
 
 output$Prev_home_cook <- data$Prev_home_cook
 output$C_home_cook    <- data$C_home_cook
+output$prob_carrier   <- data$prob_carrier
 
