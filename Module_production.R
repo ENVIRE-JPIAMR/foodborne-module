98d008ca99f647c8d50bff3ee36794604bc3d008
Subhasishbasak <subhasish.basak@anses.fr>
2024-10-02 18:59:43 +0200

    fix bug negative flock

--- Module_production.R
+++ Module_production.R
@@ -12,15 +12,21 @@ data$C_barn <- ifelse(data$B_flock_status == "p",
                       0)
 
 #Prevalence of birds internally colonized at pre-harvest(within flock prevalence)
-data$Prev_wfp_col <- ifelse(data$B_flock_status == "p",
-                            data$Prev_wfp_col_base,
-                            0)
+data$Prev_wfp_col <- data$Prev_wfp_col_base
+
+Prev_init <- ifelse(data$B_flock_status == "p",
+                    data$Prev_wfp_col_base,
+                    0)
 
 #number of bacteria on positive birds exterior at pre-harvest
 data$C_btp <- data$C_barn * data$Amount_fec 
 
 #prevalence of birds externally colonized at pre-harvest
 data$Prev_wfp_ext <- rpert(Runs, data$Prev_wfp_ext.min[1], data$Prev_wfp_col , data$Prev_wfp_ext.max[1])
+
+data$Prev_wfp_ext <- ifelse(data$B_flock_status == "p",
+                            data$Prev_wfp_ext,
+                            0)
 
 #probability of carry-over from a positive flock transported earlier on that day
 data$P_ccbf <- (1 - (1 - data$Prev_f)^data$N_transp) * data$F_cross_trans
@@ -48,7 +54,7 @@ output$B_flock_status <- data$B_flock_status
 output$load           <- data$C_barn
 output$prev           <- data$Prev_farm_type
 output$Amount_fec     <- data$Amount_fec 
-output$init_prev      <- data$Prev_wfp_col
+output$init_prev      <- Prev_init
 output$C_prod         <- data$C_prod
 output$Prev_prod      <- data$Prev_prod
 
